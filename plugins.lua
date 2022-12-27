bcdbfae2fcb3bbaaf6626d9ee03d54331955d863
Nitrogen99 <42196669+Nitrogen99@users.noreply.github.com>
2022-12-27 10:54:52 +0300

    Add files via upload

---
+++ plugins.lua
@@ -0,0 +1,14 @@
+return function (use)	
+-- Lua
+use ({
+  "folke/trouble.nvim",
+  requires = "kyazdani42/nvim-web-devicons",
+  config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+})
+end
