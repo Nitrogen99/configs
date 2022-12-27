5a26393e2757dcfc28aeab35111c198831176056
Nitrogen99 <42196669+Nitrogen99@users.noreply.github.com>
2022-12-27 10:54:02 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -136,6 +136,9 @@ vim.g.maplocalleader = ' '
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Keymap for Trouble
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", {silent = true, noremap = true})
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
